f116e5936996b4961ef7996d3b723a50085a09e3
wang0630 <j2081499@gmail.com>
2022-03-08 11:03:40 -0800

    Test hx711 with Flask

--- app/weight_scale_blueprint.py
+++ app/weight_scale_blueprint.py
@@ -1,8 +1,12 @@
 from flask import Blueprint
+from hx711py.driver import run_hx711
+# import influxdb_client
+# from influxdb_client.client.write_api import SYNCHRONOUS
 
 weight_scale_blueprint_instance = Blueprint(name='weight_scale', import_name=__name__, url_prefix='/weight')
 
 
 @weight_scale_blueprint_instance.route('/')
 def get_weight():
-    return 'get weight'
+    weight = run_hx711()
+    return f'Weight: {weight}'
