8f8dfcca944ccec8643424bdfc89c8013386cb17
wang0630 <j2081499@gmail.com>
2022-03-08 15:46:41 -0800

    Add influxdb

--- app/weight_scale_blueprint.py
+++ app/weight_scale_blueprint.py
@@ -1,8 +1,11 @@
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 from flask import Blueprint, jsonify
 from hx711py.driver import hx
-import datetime
-# import influxdb_client
-# from influxdb_client.client.write_api import SYNCHRONOUS
+
+
+client = InfluxDBClient(url="http://localhost:8086", token="uw", org="University of Washington")
+client_write_api = client.write_api(write_options=SYNCHRONOUS)
 
 weight_scale_blueprint_instance = Blueprint(name='weight_scale', import_name=__name__, url_prefix='/weight')
 
@@ -12,4 +15,13 @@ def get_weight():
     hx.GP_LOCK.acquire(blocking=True, timeout=2)
     weight = hx.val
     hx.GP_LOCK.release()
+    # Create influxdb data point
+    d = {
+        "measurement": "pet_feeder",
+        "fields": {
+            "weight": weight
+        },
+    }
+    p = Point.from_dict(d)
+    client_write_api.write(bucket="final", record=p)
     return jsonify(weight=weight)
