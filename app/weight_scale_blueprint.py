f235b893cd5d344a2657fb3b20f6dd660a4eac6c
wang0630 <j2081499@gmail.com>
2022-03-08 12:23:58 -0800

    Threading with hx711

--- app/weight_scale_blueprint.py
+++ app/weight_scale_blueprint.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
-from hx711py.driver import run_hx711
+from hx711py.driver import hx
+import datetime
 # import influxdb_client
 # from influxdb_client.client.write_api import SYNCHRONOUS
 
@@ -8,5 +9,7 @@ weight_scale_blueprint_instance = Blueprint(name='weight_scale', import_name=__n
 
 @weight_scale_blueprint_instance.route('/')
 def get_weight():
-    weight = run_hx711()
+    hx.GP_LOCK.acquire(blocking=True, timeout=2)
+    weight = hx.val
+    hx.GP_LOCK.release()
     return f'Weight: {weight}'
--- start.py
+++ start.py
@@ -1,6 +1,11 @@
+import threading
 from app import create_app
+from hx711py.driver import hx
 
 if __name__ == '__main__':
     app = create_app()
+    # Create thread for weight sensor
+    hx_thread = threading.Thread(target=hx.run_hx711, daemon=True)
+    hx_thread.start()
     # Explicitly state host = 0.0.0.0 to prevent network error
     app.run(host='0.0.0.0', port=5000, debug=True)
