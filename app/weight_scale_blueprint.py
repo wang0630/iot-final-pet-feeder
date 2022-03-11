f98fc2d16e166d19b5db968d285fea5594d0ede8
wang0630 <j2081499@gmail.com>
2022-03-11 15:10:24 -0800

    Finsihed the route

--- app/weight_scale_blueprint.py
+++ app/weight_scale_blueprint.py
@@ -4,7 +4,7 @@ from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 from influxdb_client.client.flux_table import FluxStructureEncoder
 from flask import Blueprint, jsonify, request, current_app
-from hx711py.driver import hx
+from werkzeug.exceptions import BadRequest
 
 
 client = InfluxDBClient(url="http://localhost:8086", token="uw", org="University of Washington")
@@ -26,14 +26,17 @@ def calc_mean(table):
 def get_weight():
     # Get query string
     start_time = int(request.args.get('startTime'))
+    name = request.args.get('name')
     if not start_time:
-        start_time = round(datetime.datetime.timestamp(datetime.datetime.now()))
-
+        return BadRequest('startTime should be provided')
+    if not name:
+        return BadRequest('Name should be provided')
     # Query the weight of the food at starting time
-    # tables = client_query_api.query(f'from(bucket: "final")\
-    #     |> range(start:{start_time}, stop:{start_time + 5000})\
-    #     |> filter(fn: (r) => r._measurement == "pet_feeder")\
-    # ')
+    tables = client_query_api.query(f'from(bucket: "final")\
+        |> range(start:{start_time}, stop:{start_time + 10000})\
+        |> filter(fn: (r) => r._measurement == "pet_feeder")\
+    ')
+    m_origin = calc_mean(tables[0])
 
     # Query the weight of the food for now-1m ~ now-30s and now-30s to now
     # If they are the same, write to the database
@@ -41,11 +44,30 @@ def get_weight():
             |> range(start:-2m, stop:now())\
             |> filter(fn: (r) => r._measurement == "pet_feeder")\
             |> window(every: 1m)')
+
+    # Convert FluxTable to Json
     tables_previous = json.loads(json.dumps(tables_previous, cls=FluxStructureEncoder, indent=2))
     m1 = calc_mean(tables_previous[0])
     m2 = calc_mean(tables_previous[1])
     if abs(m1 - m2) <= 200:
+        # Calculate the difference
+        m_r = m_origin - ((m2 + m2) / 2)
         # write to the database
-        return f'{m1} and {m2}'
-    return f'{m1} and {m2} is larger than 200'
-
+        d = {
+            "measurement": "pet_feeder_tags",
+            "fields": {
+                "consumed_weight": m_r
+            },
+            "tags": {
+                "name": name
+            }
+        }
+        p = Point.from_dict(d)
+        # Write to influxdb
+        client_write_api.write(bucket="final", record=p)
+        return jsonify({
+            "isFinished": True,
+        })
+    return jsonify({
+            "isFinished": False,
+        })
